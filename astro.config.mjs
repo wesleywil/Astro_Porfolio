21c4f7e2e99329b77e7a54578986bc463817d190
wesleywil <wesleywilsonti@gmail.com>
2022-09-30 15:15:58 -0300

    added translation to portuguese

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 5000,
-    host: true
+    host: true,
   },
-  integrations: [tailwind()]
+  integrations: [tailwind(), astroI18next()],
 });
